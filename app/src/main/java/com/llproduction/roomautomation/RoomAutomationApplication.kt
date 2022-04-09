bd42533429243856580d71c8f89c357dd48dbffa
Lukas <l.landsteiner@t-online.de>
2022-04-09 13:06:47 +0200

    Added Hilt

---
+++ app/src/main/java/com/llproduction/roomautomation/RoomAutomationApplication.kt
@@ -0,0 +1,8 @@
+package com.llproduction.roomautomation
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RoomAutomationApplication: Application() {
+}
