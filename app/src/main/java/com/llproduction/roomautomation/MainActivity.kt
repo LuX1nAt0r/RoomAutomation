d0ac6ea67ad84d7f7e50e4d7e8b8048258d74f6c
Lukas <l.landsteiner@t-online.de>
2022-04-09 12:38:26 +0200

    Added a button

--- app/src/main/java/com/llproduction/roomautomation/MainActivity.kt
+++ app/src/main/java/com/llproduction/roomautomation/MainActivity.kt
@@ -3,11 +3,17 @@ package com.llproduction.roomautomation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.llproduction.roomautomation.ui.theme.RoomAutomationTheme
@@ -22,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Greeting("Androd")
                 }
             }
         }
@@ -31,7 +37,26 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    var testText by remember {
+        mutableStateOf("Test")
+    }
+    
+    Column(verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+        Text(text = testText)
+
+        Button(
+
+
+            onClick = { testText = "Pressed"},
+        ) {
+            Text(text = "Press")
+
+        }
+    }
+    
+    
 }
 
 @Preview(showBackground = true)
